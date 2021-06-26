9b60db0c160206076ebc33d91e162bd651688f91
KonGkal <konsgkal@gmail.com>
2021-06-26 12:39:30 +0100

    fix: working db

--- backend/models/sequelize.ts
+++ backend/models/sequelize.ts
@@ -1,5 +1,11 @@
-import { Sequelize } from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
+import User from './User';
+import Status from './Status';
+import Mountain from './Mountain';
+import Picture from './Picture';
+import Peak from './Peak';
+import Startpoint from './Startpoint';
 
 dotenv.config();
 
@@ -19,5 +25,100 @@ const sequelize = new Sequelize(
     },
   },
 );
+
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+Status.init(
+  {
+    whishlist: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    climbed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+Mountain.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+Picture.init(
+  {
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+Peak.init(
+  {
+    latitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    elevation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+Startpoint.init(
+  {
+    latitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { sequelize },
+);
+
+User.hasMany(Status);
+Status.belongsTo(User);
+
+User.hasMany(Picture);
+Picture.belongsTo(User);
+
+Mountain.hasMany(Status);
+Status.belongsTo(Mountain);
+
+Mountain.hasMany(Picture);
+Picture.belongsTo(Mountain);
+
+Mountain.hasOne(Peak);
+
+Mountain.hasMany(Startpoint);
+Startpoint.belongsTo(Mountain);
 
 export default sequelize;
