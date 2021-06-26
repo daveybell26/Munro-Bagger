118b616a2729266b5c4a71e7c320b20929b53576
KonGkal <konsgkal@gmail.com>
2021-06-26 20:34:13 +0100

    add: getMountainById controller and route

--- backend/controllers/mountain.ts
+++ backend/controllers/mountain.ts
@@ -12,4 +12,14 @@ const getAllMountains = async (req: Request, res: Response) => {
   }
 };
 
-export default getAllMountains;
+const getMountainById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const data = await sequelize.models.Mountain.findOne({ where: { id } });
+    res.json(data);
+  } catch (e) {
+    res.json({ error: e });
+  }
+};
+
+export { getAllMountains, getMountainById };
--- backend/router.ts
+++ backend/router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import getExplore from './controllers/explore';
-import getAllMountains from './controllers/mountain';
+import { getAllMountains, getMountainById } from './controllers/mountain';
 import getUser from './controllers/user';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/user/:email', getUser);
 router.get('/mountain', getAllMountains);
 router.get('/explore', getExplore);
+router.get('./mountain/:id', getMountainById);
 
 export default router;
