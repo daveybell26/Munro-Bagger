bd89255fa4cfe25ceed502997bf37c5004264983
KonGkal <konsgkal@gmail.com>
2021-06-26 20:21:14 +0100

    add: login controller and route

--- backend/router.ts
+++ backend/router.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import getExplore from './controllers/explore';
 import getAllMountains from './controllers/mountain';
 import getUser from './controllers/user';
+import login from './controllers/login';
 
 const router = express.Router();
 
 router.get('/user/:email', getUser);
 router.get('/mountain', getAllMountains);
 router.get('/explore', getExplore);
+
+router.post('/login', login);
 
 export default router;
