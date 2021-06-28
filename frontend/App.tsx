93c4de8b27d3087ee42e271d26a8e3da989d7d0e
David Bell <david.jbell@outlook.com>
2021-06-28 19:19:17 +0100

    updated yarn.lock

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -15,7 +15,6 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar />
       <NavFooter />
     </View>
