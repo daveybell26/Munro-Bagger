93091ffdb2f5862f9404a5019f640bf0102c5e4b
KonGkal <konsgkal@gmail.com>
2021-06-28 11:35:49 +0100

    refactor: getMountainById controller to return user pictutes of the mountain

--- backend/controllers/mountain.ts
+++ backend/controllers/mountain.ts
@@ -26,7 +26,18 @@ export const getMountainById = async (req: Request, res: Response) => {
     const { id } = req.params;
     const data = await sequelize.models.Mountain.findOne({
       where: { id },
-      include: [sequelize.models.Peak],
+      include: [
+        {
+          attributes: ['id', 'latitude', 'longitude', 'elevation'],
+          model: sequelize.models.Peak,
+        },
+        {
+          model: sequelize.models.Picture,
+          order: sequelize.random(),
+          limit: 12,
+        },
+
+      ],
     });
     res.json(data);
   } catch (e) {
