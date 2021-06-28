3aaf2e417bff2b08d5e973f54fbefe1ceb7b63bb
David Bell <david.jbell@outlook.com>
2021-06-28 15:48:58 +0100

    Renamed and LF endings

---
+++ frontend/Screens/MountainProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MountainProfile = () => {
+	return (
+		<View>
+			<Text> Mountain Profile Screen</Text>
+		</View>
+	);
+};
+
+export default MountainProfile;
