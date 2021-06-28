0e12e0231b5816051015c6da00fab5ce6a0de057
David Bell <david.jbell@outlook.com>
2021-06-28 15:27:42 +0100

    Screen Repository and all screen files created

---
+++ frontend/Screens/MountainProfileScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MountainProfileScreen = () => {
+	return (
+		<View>
+			<Text> Mountain Profile Screen</Text>
+		</View>
+	);
+};
+
+export default MountainProfileScreen;
