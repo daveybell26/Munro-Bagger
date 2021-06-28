d0d43c74cca07c0e073cd34cd6554665fc8acc1e
KonGkal <konsgkal@gmail.com>
2021-06-28 17:00:31 +0100

    add: styles folder and styles for the map

--- frontend/Screens/Map.tsx
+++ frontend/Screens/Map.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+import styles from './styles/mapStyles';
 
 const Map = () => (
-  <View>
-    <Text>Map Screen</Text>
+  <View style={styles.container}>
+    <Text>Munros Map</Text>
+    <MapView style={styles.map} />
   </View>
 );
 
