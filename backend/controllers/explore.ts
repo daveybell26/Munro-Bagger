b0d7899028c79b83a14f3df1f60cc514e52182ee
Jon Brook <jonbrook89@gmail.com>
2021-06-28 09:49:39 +0100

    add raw sql query for explore random

--- backend/controllers/explore.ts
+++ backend/controllers/explore.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { Op } from 'sequelize';
+import { Op, QueryTypes } from 'sequelize';
 import sequelize from '../models/sequelize';
 
 export const getExploreUnclimbed = async (req: Request, res: Response) => {
@@ -23,23 +23,7 @@ export const getExploreUnclimbed = async (req: Request, res: Response) => {
 
 export const getExploreRandom = async (req: Request, res: Response) => {
   try {
-    const data = await sequelize.models.Picture.findAll({
-      order: sequelize.random(),
-      limit: 10,
-      attributes: ['id', 'imageUrl'],
-      include: [
-        {
-          model: sequelize.models.Mountain,
-          attributes: ['id', 'name'],
-          include: [
-            {
-              model: sequelize.models.Peak,
-              attributes: ['id', 'latitude', 'longitude'],
-            },
-          ],
-        },
-      ],
-    });
+    const data = await sequelize.query('WITH latest AS (SELECT "Pictures".*, ROW_NUMBER() OVER (PARTITION BY "Pictures"."MountainId" ORDER BY "Pictures"."createdAt" DESC) AS rn FROM "Pictures") SELECT "MountainId", "imageUrl", "createdAt" FROM latest WHERE rn = 1 ORDER BY "createdAt" DESC LIMIT 10', { type: QueryTypes.SELECT, model: sequelize.models.Pictures, mapToModel: true });
     res.json(data);
   } catch (e) {
     res.json({ error: e });
