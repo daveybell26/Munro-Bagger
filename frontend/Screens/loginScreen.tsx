fce92fdc4f4120a3aef1d0b741efc49250df87d8
David Bell <david.jbell@outlook.com>
2021-06-28 15:27:52 +0100

    Screen Repository and all screen files created

--- frontend/Screens/loginScreen.tsx
+++ frontend/Screens/loginScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const loginscreen = () => {
+const LoginScreen = () => {
 	return (
 		<View>
 			<Text> Login Screen</Text>
@@ -9,4 +9,4 @@ const loginscreen = () => {
 	);
 };
 
-export default loginscreen;
+export default LoginScreen;
