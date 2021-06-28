f4a98cdf1bdbdab57d73e987221a5aa8ba8c5a7e
KonGkal <konsgkal@gmail.com>
2021-06-28 18:04:59 +0100

    render markers from mockMonros file

--- frontend/Screens/Map.tsx
+++ frontend/Screens/Map.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Text, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import styles from './styles/mapStyles';
+import { getAllMountains } from '../mockMunros.json';
+
+const listOfMarkers = getAllMountains.map((location) => {
+  const markerLocation = {
+    latitude: location.Peak.latitude,
+    longitude: location.Peak.longitude,
+  };
+
+  return (
+    <Marker
+      onPress={() => Alert.alert(location.name)}
+      key={location.id}
+      coordinate={markerLocation}
+    />
+  );
+});
 
 const Map = () => (
   <View style={styles.container}>
     <Text>Munros Map</Text>
-    <MapView style={styles.map} />
+    <MapView style={styles.map}>
+      {listOfMarkers}
+    </MapView>
   </View>
 );
 
