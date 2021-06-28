0f300ce2a0f9a4ce5df46dfc97f01acc62840105
Jon Brook <jonbrook89@gmail.com>
2021-06-28 15:28:24 +0100

    take slice of first 6 items from the filtered data

--- backend/controllers/explore.ts
+++ backend/controllers/explore.ts
@@ -15,7 +15,8 @@ export const getExploreUnclimbed = async (req: Request, res: Response) => {
       }],
       order: sequelize.random(),
     });
-    const filteredData = data.filter((mountain: any) => mountain.Statuses[0]?.climbed !== true);
+    const filteredData = data.filter((mountain: any) => mountain.Statuses[0]?.climbed !== true)
+      .slice(0, 6);
     res.json(filteredData);
   } catch (e) {
     res.json({ error: e });
