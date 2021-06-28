09b496961434a1765030167e2b42c969e58e17af
KonGkal <konsgkal@gmail.com>
2021-06-28 18:59:47 +0100

    fix: map height to display text v0.1

--- frontend/Screens/styles/mapStyles.ts
+++ frontend/Screens/styles/mapStyles.ts
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 30,
+    height: Dimensions.get('window').height - 150,
   },
 });
 
