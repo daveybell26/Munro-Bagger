bbe92f52d66bb1c192478d722a5e41b5e739e0b3
David Bell <david.jbell@outlook.com>
2021-06-29 12:21:21 +0100

    login screen start

--- frontend/Screens/Login.tsx
+++ frontend/Screens/Login.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import {
+  SafeAreaView, Text, StyleSheet, TextInput, ImageBackground,
+} from 'react-native';
 import NavFooter from '../Components/NavFooter';
 
+const image = {};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emailInput: {
+    borderWidth: 1,
+    borderColor: '#427AA1',
+    width: 200,
+    margin: 10,
+    padding: 8,
+  },
+});
+
 const Login = () => (
-  <SafeAreaView style={{ flex: 1 }}>
+  <SafeAreaView style={styles.container}>
     <Text>Login Screen</Text>
+    <Text> Enter E-mail</Text>
+    <TextInput
+      style={styles.emailInput}
+      placeholder="example@google.com"
+    />
     <NavFooter />
   </SafeAreaView>
 );
