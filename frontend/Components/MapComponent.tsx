a47c12b6b1fe4c587b8ce9e239e557c56b73cd97
KonGkal <konsgkal@gmail.com>
2021-06-29 10:18:05 +0100

    add: redirect on mountain on marker press

--- frontend/Components/MapComponent.tsx
+++ frontend/Components/MapComponent.tsx
@@ -1,47 +1,51 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
+import { useHistory } from 'react-router-native';
 import { styles, customMap } from './styles/mapStyles';
 import { getAllMountains } from '../mockMunros.json';
 import GreenMountain from '../assets/greenMountain.png';
 import RedMountain from '../assets/redMountain.png';
 
-const listOfMarkers = getAllMountains.map((location) => {
-  const markerLocation = {
-    latitude: location.Peak.latitude,
-    longitude: location.Peak.longitude,
-  };
+const MapComponent = () => {
+  const listOfMarkers = getAllMountains.map((location) => {
+    const history = useHistory();
+    const markerLocation = {
+      latitude: location.Peak.latitude,
+      longitude: location.Peak.longitude,
+    };
+
+    return (
+      <Marker
+        onPress={() => history.push('/mountain')}
+        key={location.id}
+        coordinate={markerLocation}
+        icon={location.Statuses[0].climbed ? GreenMountain : RedMountain}
+      >
+        <Callout>
+          <Text>{location.name}</Text>
+        </Callout>
+      </Marker>
+    );
+  });
 
   return (
-    <Marker
-      // onPress={}
-      key={location.id}
-      coordinate={markerLocation}
-      icon={location.Statuses[0].climbed ? GreenMountain : RedMountain}
-    >
-      <Callout>
-        <Text>{location.name}</Text>
-      </Callout>
-    </Marker>
+    <View style={styles.container}>
+      <Text>Munros Map</Text>
+      <MapView
+        region={{
+          latitude: 57.3017,
+          longitude: -4.60763,
+          latitudeDelta: 4.5,
+          longitudeDelta: 1,
+        }}
+        style={styles.map}
+        customMapStyle={customMap}
+      >
+        {listOfMarkers}
+      </MapView>
+    </View>
   );
-});
-
-const MapComponent = () => (
-  <View style={styles.container}>
-    <Text>Munros Map</Text>
-    <MapView
-      region={{
-        latitude: 57.3017,
-        longitude: -4.60763,
-        latitudeDelta: 4.5,
-        longitudeDelta: 1,
-      }}
-      style={styles.map}
-      customMapStyle={customMap}
-    >
-      {listOfMarkers}
-    </MapView>
-  </View>
-);
+};
 
 export default MapComponent;
