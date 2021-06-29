31b25755dbee2df4771fb6c8dc9626da9d8e9eba
KonGkal <konsgkal@gmail.com>
2021-06-29 10:56:55 +0100

    refactor: destructure mountainToDisplay and declare types on useParam

--- frontend/Screens/MountainProfile.tsx
+++ frontend/Screens/MountainProfile.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { useParams } from 'react-router-native';
+import { getAllMountains } from '../mockMunros.json';
 import NavFooter from '../Components/NavFooter';
 
-const MountainProfile = () => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <Text>Mountain Profile Screen</Text>
-    <NavFooter />
-  </SafeAreaView>
-);
+const MountainProfile = () => {
+  const { id } = useParams<{ id: string }>();
+  const [mountainToDisplay] = getAllMountains.filter((mountain) => mountain.id === +id);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>{mountainToDisplay.name}</Text>
+      <NavFooter />
+    </SafeAreaView>
+  );
+};
 
 export default MountainProfile;
