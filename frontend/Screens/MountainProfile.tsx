7276830eaafe763215e6c1c1dc78e0ed91bd7010
KonGkal <konsgkal@gmail.com>
2021-06-30 15:27:59 +0100

    fix: render mountain image and info with styles

--- frontend/Screens/MountainProfile.tsx
+++ frontend/Screens/MountainProfile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import {
-  SafeAreaView, Text, StyleSheet, Image,
+  SafeAreaView, Text, StyleSheet, Image, View,
 } from 'react-native';
 import { useParams } from 'react-router-native';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,17 +15,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   title: {
-    marginTop: '10%',
     width: '100%',
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  imageContainer: {
+    flex: 0,
+  },
 });
 
 const MountainProfile = () => {
   const { id } = useParams<{ id: string }>();
-  const { name, imageUrl, Pictures } = useSelector((state:any) => state.oneMountain.mountain);
+  const {
+    name, imageUrl, Pictures, Peak, Statuses,
+  } = useSelector((state:any) => state.oneMountain.mountain);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getOneMountain(+id));
@@ -34,11 +39,28 @@ const MountainProfile = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <Header />
-      <Text style={styles.title}>{name}</Text>
-      <Image
-        source={{ uri: imageUrl }}
-        style={{ width: 200, height: 200, margin: 10 }}
-      />
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: imageUrl }}
+          style={{ width: '100%', height: 200 }}
+        />
+      </View>
+      <Text style={styles.title}>
+        Information about
+        {' '}
+        {name}
+      </Text>
+      <Text>
+        Elevation:
+        {' '}
+        {Peak?.elevation}
+        m
+      </Text>
+      <Text>
+        Status:
+        {' '}
+        {Statuses?.climbed ? 'Climbed' : 'Not Climbed'}
+      </Text>
       <ImageGrid list={Pictures} />
       <NavFooter />
     </SafeAreaView>
