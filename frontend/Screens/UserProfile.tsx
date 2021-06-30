86846f70af75d2e2976c43a71957d6038dde9d84
Rankz <ianmichaels3@live.co.uk>
2021-06-30 19:21:12 +0100

    fix: added image grid & fixed layout on user Prof

--- frontend/Screens/UserProfile.tsx
+++ frontend/Screens/UserProfile.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   SafeAreaView, Text, StyleSheet, View, Image,
 } from 'react-native';
 import NavFooter from '../Components/NavFooter';
 import Header from '../Components/Header';
+import ImageGrid from '../Components/ImageGrid';
+import { getUserPicsRandomly } from '../store/getRandomUserPics.store';
 
 const heroImage = require('../assets/hotChick.jpeg');
 
 const styles = StyleSheet.create({
+
   safeArea: {
     flex: 1,
     backgroundColor: '#fff',
   },
   title: {
-    flex: 1,
-    marginTop: '20%',
-    width: '100%',
+    // marginTop: '20%',
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
@@ -29,7 +31,7 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingBottom: 40,
     padding: 20,
-    marginTop: 35,
+    // marginTop: 35,
   },
   userImage: {
     width: 100,
@@ -42,7 +44,7 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   userMunroePics: {
-    marginTop: 5,
+    // marginTop: 5,
   },
   lineBreaks: {
     borderBottomWidth: 1,
@@ -51,41 +53,54 @@ const styles = StyleSheet.create({
   },
 });
 
-const UserProfile = () => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <Header />
-    <View style={styles.userProfileHeroSection}>
-      <Image style={styles.userImage} source={heroImage} />
-      <Text style={styles.title}>
-        Hot Chick
-      </Text>
-      <View style={{ padding: 10, backgroundColor: '#ddd', borderRadius: 50 }}>
-        <MaterialIcons name="photo-camera" size={40} />
+const UserProfile = () => {
+  const userPicList = useSelector((state:any) => state.randomUserPics.pictures);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserPicsRandomly());
+  }, [dispatch]);
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Header />
+      <View style={{ flex: 0 }}>
+        <View style={styles.userProfileHeroSection}>
+          <Image style={styles.userImage} source={heroImage} />
+          <Text style={styles.title}>
+            Hot Chick
+          </Text>
+          <View style={{ padding: 10, backgroundColor: '#ddd', borderRadius: 50 }}>
+            <MaterialIcons name="photo-camera" size={40} />
+          </View>
+        </View>
+
+        <View style={styles.lineBreaks} />
+
+        <View style={styles.userStatsSection}>
+          <Text style={styles.title}>
+            Munroes Climbed
+          </Text>
+          <Text style={styles.title}>
+            70/282
+          </Text>
+          {/* Status bar to be incooperated (dependent on userMunroeStats state) */}
+        </View>
+
+        <View style={styles.lineBreaks} />
+
+        <View style={styles.userMunroePics}>
+          <Text style={styles.title}>
+            Pictures From Mountains Climbed.
+          </Text>
+        </View>
       </View>
-    </View>
-
-    <View style={styles.lineBreaks} />
-
-    <View style={styles.userStatsSection}>
-      <Text style={styles.title}>
-        Munroes Climbed
-      </Text>
-      <Text style={styles.title}>
-        70/282
-      </Text>
-      {/* Status bar to be incooperated html CSS JavaScript (dependent on userMunroeStats state) */}
-    </View>
-
-    <View style={styles.lineBreaks} />
-
-    <View style={styles.userMunroePics}>
-      <Text style={styles.title}>
-        Pictures From Mountains Climbed.
-      </Text>
-    </View>
-    <NavFooter />
-  </SafeAreaView>
-);
+      <ImageGrid list={userPicList} />
+      <NavFooter />
+    </SafeAreaView>
+  );
+};
 
 // TODO
 // user profile hero section --> display: flexbox justify: space between
