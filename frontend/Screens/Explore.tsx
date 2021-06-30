f671a4c425d95b4d65a3b830c2af106cb53af4ae
KonGkal <konsgkal@gmail.com>
2021-06-30 16:43:10 +0100

    create exploreReducer

--- frontend/Screens/Explore.tsx
+++ frontend/Screens/Explore.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMountains } from '../store/getAllMountains.store';
+import { getRandomMountains } from '../store/explore.store';
 import NavFooter from '../Components/NavFooter';
 import Header from '../Components/Header';
 
@@ -21,11 +21,11 @@ const styles = StyleSheet.create({
 });
 
 const Explore = () => {
-  const list = useSelector((state:any) => state.allMountains.mountainList);
+  const list = useSelector((state:any) => state.exploreRandomMountains.randomMountainsList);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getMountains());
+    dispatch(getRandomMountains());
   }, [dispatch]);
 
   // TODO: remove this console.log
---
+++ frontend/store/explore.store.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getExploreRandom } from '../services/apiService';
+
+export const getRandomMountains = createAsyncThunk('randomMountains/getRandomMountains', async () => {
+  const { data } = await getExploreRandom();
+  return data;
+});
+
+interface RandomMountainsListState {
+  randomMountainsList: []
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed'
+}
+
+const initialRandomMountainsState: RandomMountainsListState = {
+  randomMountainsList: [],
+  loading: 'idle',
+};
+
+const randomMountainsSlice = createSlice({
+  name: 'randomMountains',
+  initialState: initialRandomMountainsState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getRandomMountains.pending, (state) => {
+      state.loading = 'pending';
+    });
+    builder.addCase(getRandomMountains.fulfilled, (state, action) => {
+      state.randomMountainsList = action.payload;
+      state.loading = 'succeeded';
+    });
+    builder.addCase(getRandomMountains.rejected, (state) => {
+      state.loading = 'failed';
+    });
+  },
+});
+
+export default randomMountainsSlice.reducer;
