d9350a94e85ddd6fd741d9e40425ed8ccb9e2aca
Jon Brook <jonbrook89@gmail.com>
2021-06-30 16:57:37 +0100

    first prototype of the image model

--- frontend/Components/ImageGrid.tsx
+++ frontend/Components/ImageGrid.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
-import { FlatList, Image, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  FlatList, Image, Modal, Pressable, SafeAreaView,
+} from 'react-native';
+import { BlurView } from 'expo-blur';
 
 const ImageGrid = ({ list }: { list:any }) => {
-  const singleImage = (uri: string) => (
-    <Image
-      style={{ width: 100, height: 100, margin: 10 }}
-      source={{ uri }}
-    />
+  const [modal, setModal] = useState({});
+
+  const singleImage = (item: any) => (
+    <Pressable onPress={() => setModal(item)}>
+      <Image
+        style={{
+          width: 100, height: 100, margin: 10, borderRadius: 10,
+        }}
+        source={{ uri: item.imageUrl }}
+      />
+    </Pressable>
   );
+
+  const isVisable = !!modal.id;
 
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+      <Modal visible={isVisable} animationType="fade" transparent>
+        <Pressable
+          style={{
+            flex: 1, alignItems: 'center', justifyContent: 'center',
+          }}
+          onPress={() => { setModal({}); }}
+        >
+          <BlurView
+            intensity={100}
+            tint="dark"
+            style={{
+              flex: 1, alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%',
+            }}
+          >
+            <Image
+              source={{ uri: modal.imageUrl }}
+              style={{
+                width: 300, height: 300, borderRadius: 10,
+              }}
+            />
+          </BlurView>
+        </Pressable>
+      </Modal>
       <FlatList
         data={list}
-        renderItem={({ item }) => singleImage(item.imageUrl)}
+        renderItem={({ item }) => singleImage(item)}
         keyExtractor={(item) => item.id.toString()}
         numColumns={3}
         style={{ marginTop: 10, marginBottom: 50 }}
