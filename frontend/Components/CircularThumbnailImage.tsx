8e1ca3bc07c5dbd354aad2012e2d4902223a7887
Rankz <ianmichaels3@live.co.uk>
2021-07-01 10:57:59 +0100

    feature: create CircularThumbnailImage component

---
+++ frontend/Components/CircularThumbnailImage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    padding: 0,
+    margin: 0,
+  },
+});
+
+const CircularThumbnailImage = ({ imageUrl }: { imageUrl: string }) => (
+  <Image style={styles.userImage} source={{ uri: imageUrl }} />
+);
+
+export default CircularThumbnailImage;
