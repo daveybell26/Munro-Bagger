4fa0fae0fe6e2b9ef570126cf4ca61d07223dac0
Jon Brook <jonbrook89@gmail.com>
2021-07-02 17:29:13 +0100

    add new POST and PUT endpoints for both climbed and wishlist statuses

--- backend/router.ts
+++ backend/router.ts
@@ -4,6 +4,9 @@ import { getAllMountains, getMountainById } from './controllers/mountain';
 import getRandomUserPics from './controllers/user';
 import login from './controllers/login';
 import postPicture from './controllers/picture';
+import {
+  postClimbed, postWishlist, putClimbed, putWishlist,
+} from './controllers/status';
 
 const router = express.Router();
 
@@ -14,6 +17,11 @@ router.get('/mountain/:id', getMountainById);
 
 router.get('/explore/random', getExploreRandom);
 router.get('/explore/unclimbed', getExploreUnclimbed);
+
+router.post('/status/climbed/:MountainId', postClimbed);
+router.put('/status/:id/climbed/:bool', putClimbed);
+router.post('/status/wishlist/:MountainId', postWishlist);
+router.put('/status/:id/wishlist/:bool', putWishlist);
 
 router.post('/login', login);
 
