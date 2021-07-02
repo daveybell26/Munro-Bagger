7f67aa74bc83e6567f78002dbdebc0d76eb8880b
Rankz <ianmichaels3@live.co.uk>
2021-07-02 15:46:55 +0100

    fix: Font require paths

--- frontend/Screens/styles/GlobalStyles.tsx
+++ frontend/Screens/styles/GlobalStyles.tsx
@@ -4,12 +4,12 @@ import * as Font from 'expo-font';
 import { StyleSheet } from 'react-native';
 
 Font.loadAsync({
-  NunitoSemiBold: require('../assets/fonts/Nunito-SemiBold.ttf'),
-  NunitoExtraBold: require('../assets/fonts/Nunito-ExtraBold.ttf'),
-  NunitoItalicRegular: require('../assets/fonts/Nunito-Italic.ttf'),
-  NunitoBold: require('../assets/fonts/Nunito-Bold.ttf'),
-  NunitoBlack: require('../assets/fonts/Nunito-Black.ttf'),
-  NunitoRegular: require('../assets/fonts/Nunito-Regular.ttf'),
+  NunitoSemiBold: require('../../assets/fonts/Nunito-SemiBold.ttf'),
+  NunitoExtraBold: require('../../assets/fonts/Nunito-ExtraBold.ttf'),
+  NunitoItalicRegular: require('../../assets/fonts/Nunito-Italic.ttf'),
+  NunitoBold: require('../../assets/fonts/Nunito-Bold.ttf'),
+  NunitoBlack: require('../../assets/fonts/Nunito-Black.ttf'),
+  NunitoRegular: require('../../assets/fonts/Nunito-Regular.ttf'),
 });
 
 export const globalStyles = StyleSheet.create({
