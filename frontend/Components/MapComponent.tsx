d267ec0ab9cc6c6312cf8097c7295f2e569763fb
KonGkal <konsgkal@gmail.com>
2021-07-03 13:28:27 +0100

    fix: expand mapBoundaries on the RouteMap screen to allow zoom

--- frontend/Components/MapComponent.tsx
+++ frontend/Components/MapComponent.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { SafeAreaView, Text } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { useHistory } from 'react-router-native';
-import { styles, customMap } from './styles/mapStyles';
+import styles from './styles/mapStyles';
 import { getMountains } from '../store/getAllMountains.store';
 import GreenMountain from '../assets/greenMountain.png';
 import RedMountain from '../assets/redMountain.png';
