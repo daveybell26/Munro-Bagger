48b309190629a1d487ef563bd83f8fa5fb7ae8d4
David Bell <david.jbell@outlook.com>
2021-07-07 12:13:43 +0100

    Footer Restyled with Text

--- frontend/Components/styles/navFooterStyles.tsx
+++ frontend/Components/styles/navFooterStyles.tsx
@@ -9,6 +9,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     backgroundColor: '#427AA1',
   },
+  selectedFooterText: {
+    marginTop: -4,
+    color: 'white',
+    fontSize: 10,
+  },
+  nonSelectedFooterText: {
+    marginTop: -4,
+    fontSize: 10,
+  },
 });
 
 export default styles;
